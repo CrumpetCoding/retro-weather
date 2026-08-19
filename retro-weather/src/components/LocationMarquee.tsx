7e5f170942fa211dc3049a1d0f8c230d60dfd95e
CrumpetCoding <seannrowe@gmail.com>
2026-08-19 14:13:11 +0100

    Added more info to the marquee component

--- retro-weather/src/components/LocationMarquee.tsx
+++ retro-weather/src/components/LocationMarquee.tsx
@@ -1,24 +1,26 @@
+import { useEffect, useState } from "react";
 import { Marquee } from "retro-react";
 
 
 export default function Weather() {
 
-    const [weatherData, setWeatherData] = useState<dataTypes | null>(null);
+    fetch()
+
+    const [weatherData, setWeatherData] = useState([]);
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${import.meta.env.VITE_APP_ID}`
+    const locations = ['London', 'Paris', 'New York', 'Los Angeles', 'Tokyo', 'Sydney', 'Hong Kong', 'Moscow', 'Dubai']
 
 
-    const search = async (city) => {
-        try {
-            const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${import.meta.env.VITE_APP_ID}`
-
-            const response = await fetch(url);
-            const data = await response.json();
-            setWeatherData({
+    useEffect(() => {
+        fetch(url)
+            .then(response => response.json())
+            .then(json => setWeatherData({
                 temperature: data.temperature,
                 location: data.name
-            })
+            }))
+    })
 
-        } catch (error) {
-            console.error('An error occurred:', error);
-        }
-    }
+    return (
+        <Marquee></Marquee>
+    )
 }
